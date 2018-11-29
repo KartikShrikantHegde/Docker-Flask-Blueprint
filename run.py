45fb07187e3f32ca5d0285cfe776cc047cdccde2
kartikhegde <kartikshrikanthegde@gmail.com>
2018-11-29 14:46:04 -0600

    Blueprint working - Readme added

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ load_dotenv(dotenv_path)
 
 
 config_name = os.getenv('FLASK_CONFIG')
-app = create_app(config_name)
+app = create_app()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
